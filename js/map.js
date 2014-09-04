78195d68b8e3395fbc6ecc874931fb5efc32381b
felri885 <felri885@student.liu.se>
2014-09-04 08:05:18 +0200

    added style and cdata works

--- js/map.js
+++ js/map.js
@@ -1,23 +1,57 @@
 var map;
-var marker;
+var homeMarker;
 
-var myLatlng = new google.maps.LatLng(58.405000000000000, 15.64000000000000); 
-var centerLatlng = new google.maps.LatLng(58.405, 15.640);
+var myLatlng;
+var centerLatlng;
 
 
 function initialize() { 
+  centerLatlng = new google.maps.LatLng(58.41075642311165, 15.621438785552755); //linkoping centrum
+  myLatlng = centerLatlng; //set home position to centrum by default
+
+
+  var styles = [
+  {
+    stylers: [
+      { hue: "#00ffe6" },
+      { saturation: 20 },
+      { lightness: -5  }
+    ]
+  },{
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { lightness: 100 },
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "on" }
+    ]
+  }
+];
   var mapOptions = {
     zoom: 13,
-    center: centerLatlng
+    center: centerLatlng,
+    styles: styles
   }
 
+
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  homeMarker = new google.maps.Marker({
+      map: map,
+      draggable:true,
+      position: centerLatlng,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png'
+  });
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // does not update the global variable, why???
-      console.log(myLatlng.toString());
-
+      homeMarker.setPosition(myLatlng);
     }, function() {
       handleNoGeolocation(true);
     });
@@ -25,14 +59,7 @@ function initialize() {
     // Browser doesn't support Geolocation
     handleNoGeolocation(false);
   }
-  console.log(myLatlng.toString());
 
-  marker = new google.maps.Marker({
-      map: map,
-      draggable:true,
-      animation: google.maps.Animation.DROP,
-      position: myLatlng
-  });
 
   var contentString = '<div id="content">'+
     '<div id="bodyContent">'+
@@ -44,11 +71,11 @@ function initialize() {
       content: contentString
   });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
+  google.maps.event.addListener(homeMarker, 'click', function() {
+    infowindow.open(map, homeMarker);
   });
 
-  google.maps.event.addListener(marker, 'mouseup', updatePos);
+  google.maps.event.addListener(homeMarker, 'mouseup', updatePos);
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -57,11 +84,10 @@ function handleNoGeolocation(errorFlag) {
   } else {
     console.log('Error: Your browser doesn\'t support geolocation.');
   }
-  myLatlng = new google.maps.LatLng(58.405, 15.640); 
 }
 
 function updatePos() {
-  myLatlng = new google.maps.LatLng(marker.getPosition().lat(), marker.getPosition().lng());
+  myLatlng = new google.maps.LatLng(homeMarker.getPosition().lat(), homeMarker.getPosition().lng());
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
